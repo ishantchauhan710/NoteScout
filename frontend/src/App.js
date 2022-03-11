80aa39fde5a7404dd2214b66a766da62306b004f
ishant <ishantchauhan710@gmail.com>
2022-03-11 15:26:28 +0530

    NoteCard Flexbox Grid Created

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,14 +2,11 @@ import './App.css';
 import './stylesheets/Variables.css';
 import './stylesheets/HeaderComponent.css';
 import './stylesheets/FooterComponent.css';
+import './stylesheets/CardComponent.css';
 import './stylesheets/pages/LandingPage.css';
 import './stylesheets/pages/NotesHomePage.css';
 
-
-
 import {BrowserRouter, Route} from 'react-router-dom';
-
-
 import HeaderComponent from './components/headers/HeaderComponent';
 import FooterComponent from './components/footers/FooterComponent';
 import LandingPage from './pages/LandingPage';
--- frontend/src/pages/NotesHomePage.js
+++ frontend/src/pages/NotesHomePage.js
@@ -1,4 +1,33 @@
 import React from 'react'
+import NoteComponent from '../components/cards/NoteComponent'
+
+const dummyNotes = [
+    {
+        _id: "1",
+        title: "Note 1",
+        content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        category: "Category A"
+    },
+    {
+        _id: "2",
+        title: "Note 2",
+        content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        category: "Category A"
+    },
+    {
+        _id: "3",
+        title: "Note 3",
+        content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        category: "Category B"
+    },
+    {
+        _id: "4",
+        title: "Note 4",
+        content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        category: "Category B"
+    }
+    
+];
 
 const NotesHomePage = () => {
   return (
@@ -8,13 +37,11 @@ const NotesHomePage = () => {
         </div>
 
         <div className='container-notes'>
-            <div className='note'>
-                <img className='note-image' src='https://cdn.pixabay.com/photo/2017/06/05/07/58/butterfly-2373175__340.png' />
-                <span className='note-title'>Note Title</span>
-                <span className='note-content'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</span>
-                <span className='note-category'>Category</span>
-                <span className='note-date'>7 Oct 2001, 12:00 am</span>
-            </div>
+           {
+               dummyNotes.map((note) => (
+                    <NoteComponent />
+               ))
+           }
         </div>
 
     </div>
